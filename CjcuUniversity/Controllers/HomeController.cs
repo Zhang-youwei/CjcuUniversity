c31921f2f9f7d71654da9b046ad8724706964645
CjcuUniversity <H24664034@mailst.cjcu.edu.tw>
2019-05-29 14:49:27 +0800

    Cjcu2019/05/29

--- CjcuUniversity/Controllers/HomeController.cs
+++ CjcuUniversity/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CjcuUniversity.DAL;
+using CjcuUniversity.ViewModels;
 
 namespace CjcuUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,20 @@ namespace CjcuUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
+        }
 
-            return View();
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
         public ActionResult Contact()
